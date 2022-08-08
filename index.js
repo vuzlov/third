d44c8996e1f21975a9760979419fc6210a1c8986
vuzlov yevhen <evuzlov@gmail.com>
2022-08-09 01:16:41 +0300

    add last homework

---
+++ index.js
@@ -0,0 +1,2 @@
+const name = prompt('what is your name?');
+alert ( `Hello, ${name}! How are you?`);
---
+++ index2.js
@@ -0,0 +1,4 @@
+const number1 = prompt('enter your number1');
+const number2 = prompt('enter your number2');
+let result = parseInt(number1)+parseInt(number2);
+alert ( `the sum of your numbers, ${result}`);
